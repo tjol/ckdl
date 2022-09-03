a76b708efb7758bc98a670b439d47f607c5b9abe
Thomas Jollans <tjol@tjol.eu>
2022-09-03 12:43:52 +0200

    Support Debian stretch (for the C portion)

--- src/compiler_compat.h
+++ src/compiler_compat.h
@@ -1,9 +1,9 @@
 #ifndef KDL_INTERNAL_COMPILER_COMPAT_H_
 #define KDL_INTERNAL_COMPILER_COMPAT_H_
 
-#ifdef __cplusplus
+#if defined(__cplusplus)
 #define _fallthrough_ [[fallthrough]]
-#elif __GNUC__
+#elif defined(__GNUC__) && __GNUC__ >= 7
 #define _fallthrough_ __attribute__((fallthrough))
 #else
 #define _fallthrough_
