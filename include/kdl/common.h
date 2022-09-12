a6c19a21f5340ced83596bf36736dde0fe9790c3
Thomas Jollans <tjol@tjol.eu>
2022-09-12 21:30:37 +0200

    Move function attribute macros to separate header

--- include/kdl/common.h
+++ include/kdl/common.h
@@ -2,29 +2,7 @@
 #define KDL_COMMON_H_
 
 #include <stddef.h>
-
-// define attributes for functions
-#if defined(__cplusplus)
-#    define KDL_NODISCARD [[nodiscard]]
-#elif defined(__GNUC__)
-#    define KDL_NODISCARD __attribute__((warn_unused_result))
-#else
-#    define KDL_NODISCARD
-#endif
-
-#if defined(_WIN32)
-#    ifdef BUILDING_KDL
-#        define KDL_EXPORT __declspec(dllexport)
-#    elif !defined(KDL_STATIC_LIB) || !KDL_STATIC_LIB
-#        define KDL_EXPORT __declspec(dllimport)
-#    else
-#        define KDL_EXPORT
-#    endif
-#elif defined(__GNUC__) && __GNUC__ >= 4
-#    define KDL_EXPORT __attribute__((visibility("default")))
-#else
-#    define KDL_EXPORT
-#endif
+#include "common-macros.h"
 
 #ifdef __cplusplus
 extern "C" {
