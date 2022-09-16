14ce33da73d4f13243238bee6cdf16f397ec661d
Thomas Jollans <tjol@tjol.eu>
2022-09-16 23:59:09 +0200

    Add a version number to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@ from skbuild import setup
 
 setup(
     name="ckdl",
+    version="0.1a1",
+    description="KDL parser and writer",
+    author="Thomas Jollans",
+    url="https://github.com/tjol/ckdl.git",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    license = { "file": "COPYING" },
     package_dir={"": "bindings/python/src"},
     packages=["ckdl"],
     cmake_args=[
